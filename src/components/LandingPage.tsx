9fafc3cd38cede2872432c7b010de7b5a8e1b9d6
Rick Liu <rickliu1203@gmail.com>
2025-09-01 17:40:45 -0400

    first commit

---
+++ src/components/LandingPage.tsx
@@ -0,0 +1,9 @@
+function LandingPage() {
+  return (
+    <div className='flex justify-center items-center w-screen h-screen'>
+        <h1>FujiMoji 🍎️</h1>
+    </div>
+  )
+}
+
+export default LandingPage
