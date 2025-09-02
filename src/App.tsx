96d2f60121e5f92b5caeecaa475dbc0b251bbd60
Rick Liu <rickliu1203@gmail.com>
2025-09-02 10:45:50 -0400

    add analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
 import LandingPage from './components/LandingPage'
+import { Analytics } from '@vercel/analytics/react'
 
 function App() {
 
   return (
     <>
       <LandingPage />
+      <Analytics />
     </>
      
   )
